ce149321ebbccf3538be1fdc033caec4fb226642
Seokyung Jung <tjrud1438@naver.com>
2021-05-07 20:26:39 +0900

    wip: enter key down handler

--- src/EditableBlock.js
+++ src/EditableBlock.js
@@ -1,30 +1,34 @@
 import { useState } from 'react'
-import styled from '@emotion/styled'
-import { Editor, EditorState, convertToRaw, getDefaultKeyBinding, KeyBindingUtil} from 'draft-js'
+// import styled from '@emotion/styled'
+import {
+  Editor,
+  EditorState,
+  // convertToRaw,
+  getDefaultKeyBinding
+  // KeyBindingUtil
+} from 'draft-js'
 
-const {hasCommandModifier} = KeyBindingUtil;
-
-function myKeyBindingFn(event){
-  if(event.key === 'Enter' && hasCommandModifier(event)){
+function myKeyBindingFn (event) {
+  if (event.keyCode === 13) {
     return 'make-new-block'
   }
-  return getDefaultKeyBinding(event);
+  return getDefaultKeyBinding(event)
 }
 
 const EditableBlock = ({ onEnterKeyDownHandler }) => {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   )
-  // console.log('raw', convertToRaw(editorState.getCurrentContent()))
 
-  const handleKeyCommand = (command) => {
-    if(command === 'make-new-block'){
+  const handleKeyCommand = command => {
+    if (command === 'make-new-block') {
+      onEnterKeyDownHandler()
       return 'handled'
     }
     return 'not-handled'
   }
   return (
-    <div style={{ border: '1px solid red', height: '30px'}}>
+    <div style={{ border: '1px solid red', height: '30px' }}>
       <Editor
         editorState={editorState}
         onChange={setEditorState}
@@ -32,7 +36,6 @@ const EditableBlock = ({ onEnterKeyDownHandler }) => {
         keyBindingFn={myKeyBindingFn}
       />
     </div>
-   
   )
 }
 
